1985a2dcec1711025b8c9901128de68a1f8833ec
Jellyfrog <Jellyfrog@users.noreply.github.com>
2020-04-03 18:41:43 +0200

    Update pre-commit.php

--- scripts/pre-commit.php
+++ scripts/pre-commit.php
@@ -232,7 +232,7 @@ function run_check($type, $passthru, $command_only, $options = array())
 {
     global $completed_tests;
     var_dump($completed_tests);
-    var_dump(getenv('SKIP_' . strtoupper($type) . '_CHECK'));    
+    var_dump(getenv('SKIP_' . strtoupper($type) . '_CHECK'));
     if (getenv('SKIP_' . strtoupper($type) . '_CHECK') || $completed_tests[$type]) {
         echo ucfirst($type) . " check skipped.\n";
         return 0;
