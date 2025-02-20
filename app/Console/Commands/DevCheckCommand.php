a8cf9fd73279a2b0edcf9d9719fb0a8d58107f3c
Jellyfrog <Jellyfrog@users.noreply.github.com>
2025-02-21 00:22:21 +0100

    wip

--- app/Console/Commands/DevCheckCommand.php
+++ app/Console/Commands/DevCheckCommand.php
@@ -51,7 +51,7 @@ class DevCheckCommand extends LnmsCommand
         $this->addOption('snmpsim', null, InputOption::VALUE_NONE);
         $this->addOption('full', null, InputOption::VALUE_NONE);
         $this->addOption('os-modules-only', null, InputOption::VALUE_NONE);
-        $this->addOption('exclude-group', null, InputOption::VALUE_NONE);
+        $this->addOption('exclude-group', null, InputOption::VALUE_REQUIRED);
         $this->addOption('commands', 'c', InputOption::VALUE_NONE);
     }
 
