bd7505eeb1d8ceb0b1e6a5ff8a6e840a27db8fee
Jellyfrog <Jellyfrog@users.noreply.github.com>
2025-03-20 01:36:52 +0100

    wip

--- LibreNMS/Util/CiHelper.php
+++ LibreNMS/Util/CiHelper.php
@@ -184,7 +184,7 @@ class CiHelper
      */
     public function checkUnit(): int
     {
-        $phpunit_cmd = [$this->checkPhpExec('phpunit'), '--colors=always', '--testdox', '--no-progress'];
+        $phpunit_cmd = [$this->checkPhpExec('phpunit'), '--colors=always', '--no-progress'];
 
         if ($this->flags['fail-fast']) {
             array_push($phpunit_cmd, '--stop-on-error', '--stop-on-failure');
